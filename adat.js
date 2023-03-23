85336847374b2338a0535617005daa0712547d1e
BeldyAdam <115634783+BeldyAdam@users.noreply.github.com>
2023-03-23 18:26:45 +0100

    Add files via upload

--- adat.js
+++ adat.js
@@ -3,6 +3,17 @@ export const SZAMLISTA=[11,12,13,14,15]
 export const SZOVEGLISTA=["asd","vbn","kle"]
 
 export const OBJEKTUMLISTA=[
-    {nev:"Dézi",fajta:"keverék"},
-    {nev:"Lorant",fajta:"korcs"},
+    {nev:"Dézi",fajta:"keverék", kor: 15},
+    {nev:"Lorant",fajta:"korcs", kor: 3},
+    {nev:"Keala",fajta:"golden", kor: 5},
+    {nev:"Makani",fajta:"bernáthegyi", kor: 2},
+    {nev:"Nana",fajta:"nemtom", kor: 10},
+]
+
+export const KOR=[
+    {nev:"Dézi",fajta:"keverék", kor: 15},
+    {nev:"Lorant",fajta:"korcs", kor: 3},
+    {nev:"Keala",fajta:"golden", kor: 5},
+    {nev:"Makani",fajta:"bernáthegyi", kor: 2},
+    {nev:"Nana",fajta:"nemtom", kor: 10},
 ]
--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 import { SZAMLISTA } from "./adat.js";
 import { SZOVEGLISTA } from "./adat.js";
+import { OBJEKTUMLISTA } from "./adat.js";
+import { KOR } from "./adat.js";
 
-
-/* console.log(SZAMLISTA); */
-/* console.log(SZOVEGLISTA); */
 $(function () {
   /*    let kevertIndexLista = keveres1(SZAMLISTA);
     console.log(kevertIndexLista)
@@ -11,11 +10,28 @@ $(function () {
   /*     for (let index = 0; index < SZAMLISTA.length; index++) {
         console.log(SZAMLISTA[kevertIndexLista[index]])
     }  */
-  console.log(SZOVEGLISTA);
+/*   console.log(SZOVEGLISTA);
   keveres2(SZOVEGLISTA);
   console.log(SZOVEGLISTA);
-  keveres3(SZOVEGLISTA);
+  keveres3(SZOVEGLISTA); */
+
+  console.log(OBJEKTUMLISTA);
+  console.log("Név szerint");
+  console.log(nevRendezes(OBJEKTUMLISTA));
+  console.log("Kor szerint");
+  console.log(korRendezes(KOR));
 });
+
+
+function nevRendezes(lista){
+  let rendezettNevek = lista.sort();
+  return rendezettNevek;
+}
+
+function korRendezes(lista){
+  let kor = lista.sort((a, b) => a.kor - b.kor);
+  return kor;
+}
 
 function keveres1(lista) {
   let megkevertIndex = [];
